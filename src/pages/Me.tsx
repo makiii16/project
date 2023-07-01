ef73d4029002e6431d6c93fdfc41e870b4488916
makiii16 <102356903+makiii16@users.noreply.github.com>
2023-07-01 10:36:43 +0200

    Update Me.tsx

--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -4,11 +4,12 @@ import {UserDto} from "../classes/user.dto";
 const Me = ({user}:{user: UserDto}) => {
     return (
         <>
-            <h1>Moji podatki</h1>
+            <h1>Tvoji podatki</h1>
             <h3>Ime: {user.first_name}</h3>
             <h3>Priimek: {user.last_name}</h3>
             <h3>Email: {user.email}</h3>
         </>
     );
 }
+
 export default Me;
